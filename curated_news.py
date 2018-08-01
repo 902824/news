7f88fdc3d158de6fc28cb79adedcece4a88a7f99
Zack <zreynold@andrew.cmu.edu>
2018-08-01 17:29:50 -0400

    pretty good one

--- curated_news.py
+++ curated_news.py
@@ -3,7 +3,7 @@ import feedparser
 from random import randint
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
-
+import random
 # import feedparser
 
 app = Flask(__name__)
@@ -18,29 +18,64 @@ def new_feed():
     welcome_msg = render_template('task')
     return question(welcome_msg)
 
+@ask.intent("AMAZON.StopIntent")
+def stop():
+    return statement("okay")
 
 @ask.intent("TopicIntent", convert={'topicResponse': str})
 def read_hlines(topicResponse):
+    word = ""
     session.attributes['state'] += 1
     if (session.attributes['state'] == 1):
+       
+        if(topicResponse.lower() == "basketball" or topicResponse.lower() == "football"):
+            word = "Pro "+topicResponse
+            session.attributes['nameTopic'] = camelcase(word)
+        elif (topicResponse.lower() == "nfl"):
+            session.attributes['nameTopic'] = camelcase("football")
+        elif (topicResponse.lower() == "nba" or topicResponse.lower() == "n yeah"):
+            session.attributes['nameTopic'] = camelcase("Pro Basketball")
+        elif (topicResponse.lower() == "news"):
+            ran = random.randint(0,14)
+            articles = ["World", "Africa", "Americas", "Politics", "Sports", "Space", "Movies", "Travel", "Golf", "Baseball", "Soccer", "Science", "Education", "Technology"]
+            session.attributes['nameTopic'] = camelcase(articles[ran])
         
-        feed = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/' + topicResponse + '.xml')
-        session.attributes['nameTopic'] = topicResponse
+        else:
+            session.attributes['nameTopic'] = camelcase(topicResponse)
+        
+        try:
+            feed = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/' + session.attributes['nameTopic'] + '.xml')
+            news1 = feed.entries[1].title
+            #print(session.attributes['state']+ "try")
+        except:
+            session.attributes['state'] == 0
+            new = "Sorry, {} is not an option"
+            
+            #print(session.attributes['state'])
+            
+            msg = new.format(session.attributes['nameTopic'])
+            return question(msg)
+        feed = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/' + session.attributes['nameTopic'] + '.xml')
+                
         news1 = feed.entries[1].title
         news2 = feed.entries[2].title
-        new = " here is the top headlines,,1,. {},. 2,,, {},. what do you want to hear about?"
-        msg = new.format(news1, news2)
+        news3 = feed.entries[3].title
+        new = " here's the top headlines for {},,1,. {},. 2,,, {}, and, 3, {},. what number do you want to hear about?"
+        msg = new.format(session.attributes['nameTopic'], news1, news2, news3)
         return question(msg)
     if (session.attributes['state'] == 2):
         msg = read_article(topicResponse)
-        return question(msg)
+        return statement(msg)
     if (session.attributes['state'] == 3):
         msg = "Thank you"
         return statement(msg)
+        
+
+            
 def cutWord(sentence):
     fillers = ["um", "uh", "like", "hi", "hey", "hello", "oh"]
     pronouns = ["i", "we", "you", "he", "she", "it", "they", "his", "her", "their", "my", "our", "your", "me", "us", "him", "her", "them"]
-    determiners = ["a", "and", "the", "this", "that", "these", "those"]
+    determiners = ["a", "and", "the", "this", "that", "these", "those", "an"]
     verbs = ["to be", "am", "are", "is"]
     for word in list(sentence): 
         if word in fillers:
@@ -55,7 +90,13 @@ def cutWord(sentence):
         if word in verbs:
             sentence.remove(word)
     return sentence
-
+    
+def camelcase(s):
+    s = s.title()
+    words = s.split()
+    words = cutWord(words)
+    title = "".join(words)
+    return title
 
 def read_article(number):
     '''
@@ -66,11 +107,19 @@ def read_article(number):
     return statement(article_msg)
     '''
     feed = feedparser.parse('http://rss.nytimes.com/services/xml/rss/nyt/' + session.attributes['nameTopic'] + '.xml')
-    if(number == "one" or number == "1"):
+    if(number.lower() == "one" or number == "1" or "first" in number.lower() or "1" in number.lower()):
         feeds = feed.entries[1].summary
-    else:
+    elif(number.lower() == "two" or number == "2" or "second" in number.lower() or "mid" in number.lower() or "middle" in number.lower() or "2" in number.lower()):
         feeds = feed.entries[2].summary
+    elif(number.lower() == "three" or number == "3" or "last" in number.lower() or "last one" in number.lower()) or "3" in number.lower() or "final" in number.lower():
+        feeds = feed.entries[3].summary
+    else:
+        feeds = "Sorry, that was not an option"
+        
     msg = format(feeds)
+    blank = msg.strip()
+    if(len(blank) < 2):
+        return "Sorry, there was no summary"
     return ","+msg
 
 if __name__ == '__main__':
